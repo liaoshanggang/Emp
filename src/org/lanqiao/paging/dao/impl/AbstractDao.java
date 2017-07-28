0f1daa80c1ab4e74034e6c5d28537f1e6d4713f5
liaoshanggang <787887060@qq.com>
2017-07-28 22:13:56 +0800

    正确的适配器

--- src/org/lanqiao/paging/dao/impl/AbstractDao.java
+++ src/org/lanqiao/paging/dao/impl/AbstractDao.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.lanqiao.paging.dao.BaseDao;
 import org.lanqiao.paging.util.DBUtil;
@@ -32,5 +33,41 @@ public abstract class AbstractDao<T, PK extends Serializable> implements BaseDao
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	@Override
+	public List<T> select(int pageSize, int currentPage) {
+		return null;
+	}
+	@Override
+	public List<T> select(int pageSize, int currentPage, T obj) {
+		return null;
+	}
+	@Override
+	public List<T> select() {
+		return null;
+	}
+	@Override
+	public T select(PK id) {
+		return null;
+	}
+	@Override
+	public void insert(T t) {
+		
+	}
+	@Override
+	public void delete() {
+		
+	}
+	@Override
+	public void delete(PK id) {
+		
+	}
+	@Override
+	public void update(T t) {
+		
+	}
+	@Override
+	public int count() {
+		return 0;
 	}
 }
